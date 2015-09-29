7a332da726c42211b8ca88548554f7fa4a183d4f
Viet Vu <jooservices@gmail.com>
2015-09-29 09:04:37 +0700

    ^ uppdate plg com_content  docblock

--- plugins/ztmap/com_content/com_content.php
+++ plugins/ztmap/com_content/com_content.php
@@ -1,12 +1,24 @@
 <?php
 
 /**
- * @version             $Id$
- * @copyright           Copyright (C) 2007 - 2009 Joomla! Vargas. All rights reserved.
- * @license             GNU General Public License version 2 or later; see LICENSE.txt
- * @author              Guillermo Vargas (guille@vargas.co.cr)
+ * @version       $Id$
+ * @copyright     Copyright (C) 2007 - 2009 Joomla! Vargas. All rights reserved.
+ * @license       GNU General Public License version 2 or later; see LICENSE.txt
+ * @author        Guillermo Vargas (guille@vargas.co.cr)
+ * 
+ * @name        Zt Map
+ * @version     0.0.5
+ * @package     Joomla
+ * @subpackage  Component
+ * @author      ZooTemplate 
+ * @email       support@zootemplate.com 
+ * @link        http://www.zootemplate.com 
+ * @copyright   Copyright (c) 2015 ZooTemplate
+ * @license     GPL v2 
+ * 
  */
-defined('_JEXEC') or die('Restricted access');
+// no direct access
+defined('_JEXEC') or die;
 
 require_once JPATH_SITE . '/components/com_content/helpers/route.php';
 require_once JPATH_SITE . '/components/com_content/helpers/query.php';
